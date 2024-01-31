00d7f64242c02bef4b8e55f2711c2091d3be8d53
Neetika Aryal <neetikaaryal16@gmail.com>
2024-01-31 11:29:50 +0545

    Readd home page route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/home', function () {
+//     Route::get('/home', [HomeController::class, 'home'])->name('home');
+//     // return view('welcome');
+//     return redirect('/home');
+// });
+Route::get('/home' , HomeController::class . '@home')->name('home');
+
+// ---------Blog------------
+Route::get('/blog' , HomeController::class . '@blog')->name('blog');
+
+
+Route::get('/create', function() {
+    return view('create');
 });
+
+Route::post('/store-blog', HomeController::class. '@store')->name('store-blog');
+
+// deletes a post
+Route::get('/delete/{id}', HomeController::class .'@delete')->name('delete');
+
+// returns the form for editing a post
+Route::get('/edit/{id}', HomeController::class .'@edit')->name('edit');
+
+
+
+// returns the home page with all posts
+// Route::get('/', HomeController::class .'@index')->name('posts.index');
+// returns the form for adding a post
+Route::get('/posts/create', HomeController::class . '@create')->name('posts.create');
+// adds a post to the database
+Route::post('/posts', HomeController::class .'@store')->name('posts.store');
+// returns a page that shows a full post
+Route::get('/posts/{post}', HomeController::class .'@show')->name('posts.show');
+
+// updates a post
+Route::get('/update/{id}', HomeController::class .'@update')->name('update');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
