218bc044952926a8cdc06f65e74dfb6073cf4f82
Neetika Aryal <neetikaaryal16@gmail.com>
2024-02-07 12:28:27 +0545

    Api path

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("data", [ApiController::class, 'getData']);
+Route::get('/data', [ApiController::class, 'getData']);
